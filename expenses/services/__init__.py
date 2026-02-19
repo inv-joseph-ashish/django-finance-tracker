fe2fac2a8b64241d1033accf7fd02c551b895c2c
Ashish <joseph.ashish@innovaturelabs.com>
2026-02-19 18:01:09 +0530

    revert

--- expenses/services/__init__.py
+++ expenses/services/__init__.py
@@ -1,4 +1,4 @@
 from .bill_scanner import scan_bill_image
 from .services import BalanceCalculationService
 
-__all__ = ["scan_bill_image", "BalanceCalculationService"] 
+__all__ = ["scan_bill_image", "BalanceCalculationService"]
